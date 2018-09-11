18d4024b27e49016b0c1f593f58f9a44d531215b
Jobi Mathai <jobi.mathai@gmail.com>
2018-09-10 23:19:35 -0700

    added jquery stuff to see if that will work

--- assets/javascript/game-1.js
+++ assets/javascript/game-1.js
@@ -5,8 +5,6 @@ var alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var compAlphabet = alphabets[Math.floor(Math.random() * alphabets.length)];
 console.log(compAlphabet);
 
-
-var guessedAlphabet = [];
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -23,7 +21,7 @@ document.onkeypress = function (event) {
         wins++;
         guesses--;
 
-    //If user's guess is incorrect, decrease the guess count
+        //If user's guess is incorrect, decrease the guess count
     } else {
         guesses--;
     }
@@ -34,68 +32,26 @@ document.onkeypress = function (event) {
         guesses = 9;
     }
 
-    // function addGuess() {
-    //     var inp = document.getElementById('guessedAlphabet');
-    //     guessedAlphabet.push(inp.value);
-    //     inp.value = "";
-    //         }
-
-    // function showGuess(){
-    //     document.getElementById("guessAlphabet").innerHTML = guessedAlphabet.join(", ");
-    // }
-
-   //loop through the user's input & put it in an array.
-    for (var j = 0; j < 9; j++){
+    //loop through the user's input & put it in an array.
+    for (var j = 0; j < 9; j++) {
         console.log(userGuess);
-    document.getElementById("guessedAlphabet").appendChild(userGuess);
+        var userEnter = $("<guessedLetter>");
+        userEnter.addClass("alpha")
+        userEnter.attr("data-letter", j[j])
+        userEnter.text(j[j]);
+        $("guessedAlphabet").append(userEnter)
     }
 
     
+    $("#guessedAlphabet").on("keyup", function () {
+        var guessedAlpha = $("<div>")
+        guessedAlpha.addClass("letter")
+        guessedAlpha.text($(this).attr("data-letter"))
+        $("guessedAlphabet".append(guessedAlpha))
+        console.log(guessedAlpha);
+    })
 
     document.getElementById('winCount').innerHTML = wins;
     document.getElementById('lossCount').innerHTML = losses;
     document.getElementById('guessCount').innerHTML = guesses;
-    
 }
-
-// function resetGame() {
-//     var guesses = 9;
-//     var compAlphabet = alphabets[Math.floor(Math.random() * alphabets.length)];
-//     return(compAlphabet);
-// }
-
-// // You're going to make a game just like the one in the video. Essentially, the app randomly picks a letter, and the user has to guess which letter the app chose. Put the following text on your page:
-// // Guess what letter I'm thinking of
-// // Wins: (# of times the user has guessed the letter correctly)
-
-
-// // Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
-// // Guesses Left: (# of guesses left. This will update)
-
-// guessCount: var guessCounter = function(guessCount){
-
-// for (var i = 9; i > 1; i-=1) {
-// 	guesses[i]
-//     }
-
-// }
-// // When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-// winCount: var winCounter = function(winCount){
-
-//     if ((userGuess === compAlphabet)) {
-//     for (var j = 0; j < 1; j++){
-//     wins + 1;
-//         console.log(wins);
-//     } else guessCounter;
-// }
-// }
-// // When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
-// lossCount: var lossCounter = function(lossCount){
-//     for (var k = 0; k < 1; k++){
-//          = 
-//     }
-// }
-
-// // Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
-
-
